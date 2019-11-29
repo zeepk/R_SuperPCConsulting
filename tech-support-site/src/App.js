5b218fd5038529e7c17fd6a2af826815bbc27c3b
Matthew Hughes <matthew.l.hughes@kp.org>
2019-11-29 13:08:37 -0800

    fontawesome

--- tech-support-site/src/App.js
+++ tech-support-site/src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "./App.css";
+import {
+  faHome,
+  faCloudUploadAlt,
+  faBriefcase,
+  faUserCheck,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
@@ -10,7 +18,36 @@ function App() {
       </header>
       <body>
         <br></br>
-        <img src={require("./images/laptop1.jpeg")} alt="Laptop"></img>
+        <br></br>
+        <div class="NavGrid">
+          <div class="NavBox a"></div>
+
+          <div class="NavBox b">
+            <FontAwesomeIcon icon={faBriefcase} size={"3x"} />
+            <br />
+            Business
+          </div>
+
+          <div class="NavBox c">
+            <FontAwesomeIcon icon={faHome} size={"3x"} />
+            <br />
+            Residential
+          </div>
+
+          <div class="NavBox d">
+            <FontAwesomeIcon icon={faCloudUploadAlt} size={"3x"} />
+            <br />
+            Cloud Service
+          </div>
+
+          <div class="NavBox e">
+            <FontAwesomeIcon icon={faUserCheck} size={"3x"} />
+            <br />
+            Data Recovery Support
+          </div>
+
+          <div class="NavBox f"></div>
+        </div>
       </body>
     </div>
   );
