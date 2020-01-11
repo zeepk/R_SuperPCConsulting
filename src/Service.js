ab75867daf44e342215ffb13af1fa2cd5dd7e98c
Matt Hughes <matthughes@Matts-MacBook-Pro.local>
2020-01-10 18:36:24 -0800

    new picture and nav format

--- src/Service.js
+++ src/Service.js
@@ -1,39 +1,32 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Spring } from "react-spring/renderprops";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 var innerStyle = {
-  padding: "3vh 0",
-  fontSize: "1.8vh"
+	padding: '3vh 0',
+	fontSize: '1.8vh'
 };
 
 export default function Service(props) {
-  var circleStyle = {
-    padding: 10,
-    margin: 20,
-    display: "inline-block",
-    backgroundColor: props.circleColor,
-    borderRadius: "50%",
-    width: "20vh",
-    height: "20vh"
-  };
-  return (
-    <Spring
-      from={{ opacity: 0, marginTop: -500 }}
-      to={{ opacity: 1, marginTop: 0 }}
-    >
-      {props2 => (
-        <div style={props2}>
-          <div style={circleStyle}>
-            <div style={innerStyle}>
-              <FontAwesomeIcon icon={props.iconName} size={"3x"} />
-              <br />
-              <br />
-              <h2>{props.serviceName}</h2>
-            </div>
-          </div>
-        </div>
-      )}
-    </Spring>
-  );
+	var circleStyle = {
+		padding: 10,
+		margin: 20,
+		display: 'inline-block',
+		backgroundColor: '#6aa8c3',
+		borderRadius: '50%',
+		width: '25vh',
+		height: '25vh'
+	};
+	var textBoxStyle = {};
+	return (
+		<div>
+			<div style={circleStyle}>
+				<div style={innerStyle}>
+					<FontAwesomeIcon icon={props.iconName} size={'4x'} />
+					<br />
+					<br />
+					<h2>{props.serviceName}</h2>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  const linkStyle = {
-    color: "#fff",
-    textDecoration: "none",
-    textAlign: "center"
-  };
+	const linkStyle = {
+		color: 'black',
+		textDecoration: 'none',
+		textAlign: 'center'
+	};
 
-  return (
-    <div>
-      <header>
-        <p className="Phone">Call 925-867-5309</p>
-        <Link to="/" style={linkStyle}>
-          <h1>Super PC Consulting</h1>
-        </Link>
-        <h4>Computer Consulting and Repair</h4>
-        <br />
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<p className="Phone">Call 925-867-5309</p>
+				<Link to="/" style={linkStyle}>
+					<h1>Super PC Consulting</h1>
+				</Link>
+				<h4>Computer Consulting and Repair</h4>
+				<br />
+			</header>
+		</div>
+	);
 }
