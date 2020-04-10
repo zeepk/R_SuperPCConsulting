fb46f380289cdf82ea66d1a27895e66c1f001c64
Matt <matt@matthughes.dev>
2020-04-09 20:32:35 -0700

    animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import Slide from 'react-reveal/Slide';
 export default function Header() {
 	return (
-		<div>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a
-					class="navbar-brand"
-					href="#"
-					style={{ fontWeight: 'bold', fontSize: '3vh' }}
-				>
-					Super PC Consulting
-				</a>
-				<button
-					class="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span class="navbar-toggler-icon"></span>
-				</button>
+		<Slide right>
+			<div>
+				<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+					<a
+						class="navbar-brand"
+						href="/"
+						style={{ fontWeight: 'bold', fontSize: '3vh' }}
+					>
+						Super PC Consulting
+					</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">
-								Home <span class="sr-only">(current)</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								Business
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								Residential
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								Cloud Service
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								Data Recovery
-							</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="/">
+									Home <span class="sr-only">(current)</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/business">
+									Business
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/residential">
+									Residential
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/cloudservice">
+									Cloud Service
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/datarecovery">
+									Data Recovery
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</Slide>
 	);
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,6 +8,7 @@ import {
 	faCloudUploadAlt,
 	faUserCheck,
 } from '@fortawesome/free-solid-svg-icons';
+import Flip from 'react-reveal/Flip';
 export default class Home extends Component {
 	render() {
 		return (
@@ -18,62 +19,64 @@ export default class Home extends Component {
 					alt="Laptop"
 				/>
 				<div className="p-grid">
-					<div className="p-col-12 p-md-6 p-lg-3">
-						<Panel header="Business">
-							<FontAwesomeIcon
-								className="icon"
-								icon={faBriefcase}
-								size={'8x'}
-							/>
-							<p>
-								As a small business, you may not need a lot of IT support. But
-								when you do, you need it NOW. Get ahead of the game with a
-								managed service contract with 24/hr support.
-							</p>
-						</Panel>
-					</div>
-					<div className="p-col-12 p-md-6 p-lg-3">
-						<Panel header="Residential">
-							<FontAwesomeIcon className="icon" icon={faHome} size={'8x'} />
-							<p>
-								Is your computer running slowly or crashing? We offer complete
-								home solutions, virus/spyware removal, data transfer,
-								networking, training and buying assistance offered at a
-								reasonable price.
-							</p>
-						</Panel>
-					</div>
-					<div className="p-col-12 p-md-6 p-lg-3">
-						<Panel header="Cloud Service">
-							<FontAwesomeIcon
-								className="icon"
-								icon={faCloudUploadAlt}
-								size={'8x'}
-							/>
-							<p>
-								Simplify your IT infrastructure! Save on IT cost, access your
-								data anywhere, such as:
-							</p>
-							<ul>
-								<li>iCloud</li>
-								<li>Office365</li>
-								<li>Google Apps</li>
-							</ul>
-						</Panel>
-					</div>
-					<div className="p-col-12 p-md-6 p-lg-3">
-						<Panel header="Data Recovery">
-							<FontAwesomeIcon
-								className="icon"
-								icon={faUserCheck}
-								size={'8x'}
-							/>
-							<p>
-								Dead hard drive? All is not lost. We can help with full data
-								recovery
-							</p>
-						</Panel>
-					</div>
+					<Flip left cascade>
+						<div className="p-col-12 p-md-6 p-lg-3">
+							<Panel header="Business">
+								<FontAwesomeIcon
+									className="icon"
+									icon={faBriefcase}
+									size={'6x'}
+								/>
+								<p>
+									As a small business, you may not need a lot of IT support. But
+									when you do, you need it NOW. Get ahead of the game with a
+									managed service contract with 24/hr support.
+								</p>
+							</Panel>
+						</div>
+						<div className="p-col-12 p-md-6 p-lg-3">
+							<Panel header="Residential">
+								<FontAwesomeIcon className="icon" icon={faHome} size={'6x'} />
+								<p>
+									Is your computer running slowly or crashing? We offer complete
+									home solutions, virus/spyware removal, data transfer,
+									networking, training and buying assistance offered at a
+									reasonable price.
+								</p>
+							</Panel>
+						</div>
+						<div className="p-col-12 p-md-6 p-lg-3">
+							<Panel header="Cloud Service">
+								<FontAwesomeIcon
+									className="icon"
+									icon={faCloudUploadAlt}
+									size={'6x'}
+								/>
+								<p>
+									Simplify your IT infrastructure! Save on IT cost, access your
+									data anywhere, such as:
+								</p>
+								<ul>
+									<li>iCloud</li>
+									<li>Office365</li>
+									<li>Google Apps</li>
+								</ul>
+							</Panel>
+						</div>
+						<div className="p-col-12 p-md-6 p-lg-3">
+							<Panel header="Data Recovery">
+								<FontAwesomeIcon
+									className="icon"
+									icon={faUserCheck}
+									size={'6x'}
+								/>
+								<p>
+									Dead hard drive? All is not lost. We can help with full data
+									recovery
+								</p>
+							</Panel>
+						</div>
+					</Flip>
 				</div>
 			</div>
 		);
